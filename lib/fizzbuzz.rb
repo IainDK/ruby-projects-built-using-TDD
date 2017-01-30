ef51e8a119a685e42e3550ac3e426bfaece2fc9a
Iain Kinnish <iainkinnish@googlemail.com>
2017-01-30 16:49:38 +0000

    Exercixe #1: FizzBuzz. First failing test.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 class FizzBuzz
 
+def divisble_by_three?(number)
+  true
+end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,16 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do
+  let(:game) { (FizzBuzz.new) }
+
+  describe 'divisble_by_three?(number)' do
+    it 'expects that if the supplied number is divisible by three, the result will be true' do
+      expect(game.divisble_by_three?(9)).to be true
+    end
+
+    it 'expects that if the supplied number is not divisible by three, the result will be false' do
+      expect(game.divisble_by_three?(5)).to be false
+    end
+  end
 
 end
