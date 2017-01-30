fa3175306fb0b52259a86cce517568ef8af30fc2
Iain Kinnish <iainkinnish@googlemail.com>
2017-01-30 17:39:55 +0000

    All tests passing. FizzBuzz can now be played & works as intended.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,31 @@
 class FizzBuzz
 
+def play(number)
+  if divisible_by_fifteen?(number)
+    'FizzBuzz'
+  elsif divisible_by_five?(number)
+    'Buzz'
+  elsif divisble_by_three?(number)
+    'Fizz'
+  else
+    number
+  end
+end
+
 def divisble_by_three?(number)
-  number % 3 == 0
+  divisible_by?(number, 3)
 end
 
 def divisible_by_five?(number)
-  number % 5 ==0
+  divisible_by?(number, 5)
+end
+
+def divisible_by_fifteen?(number)
+  divisible_by?(number, 15)
+end
+
+def divisible_by?(number, divisor)
+  number % divisor == 0
 end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,28 @@ require 'fizzbuzz'
 describe FizzBuzz do
   let(:game) { (FizzBuzz.new) }
 
+  describe 'play(number)' do
+    it 'expects that if the supplied number is divisible by fifteen, the result will be FizzBuzz' do
+      expect(game.play(30)).to eq 'FizzBuzz'
+    end
+
+    it 'expects that if the supplied number is divisible by five, the result will be Buzz' do
+      expect(game.play(10)).to eq 'Buzz'
+    end
+
+    it 'expects that if the supplied number is divisible by three, the result will be Fizz' do
+      expect(game.play(9)).to eq 'Fizz'
+    end
+
+    it 'expects that if the supplied number is not divisible by three, five or fifteen, the number itself will be returned.' do
+      expect(game.play(8)).to eq 8
+    end
+
+    it 'expects that if the supplied number is not divisible by three, the result will be false' do
+      expect(game.divisble_by_three?(5)).to be false
+    end
+  end
+
   describe '#divisble_by_three?(number)' do
     it 'expects that if the supplied number is divisible by three, the result will be true' do
       expect(game.divisble_by_three?(9)).to be true
@@ -20,6 +42,26 @@ describe FizzBuzz do
 
     it 'expects that if the supplied number is not divisible by five, the result will be false' do
       expect(game.divisible_by_five?(7)).to be false
+    end
+  end
+
+  describe '#divisble_by_fifteen?(number)' do
+    it 'expects that if the supplied number is divisible by fifteen, the result will be true' do
+      expect(game.divisible_by_fifteen?(30)).to be true
+    end
+
+    it 'expects that if the supplied number is not divisible by fifteen, the result will be false' do
+      expect(game.divisible_by_fifteen?(17)).to be false
+    end
+  end
+
+  describe '#divisble_by?(number, divisor)' do
+    it 'expects that if the supplied number is divisible by the divisor, the result will be true' do
+      expect(game.divisible_by?(20, 5)).to be true
+    end
+
+    it 'expects that if the supplied number is not divisible by the divisor, the result will be false' do
+      expect(game.divisible_by?(9, 4)).to be false
     end
   end
 end
